5360fc8b961679e27aab7d573baa3d5af97cf875
Kelly <lykelly2014@icloud.com>
2020-06-20 11:35:13 -0400

    initial code

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def receive_sms():
+    body = request.values.get('Body', None)
+    resp = MessagingResponse()
+    resp.message(body or 'Hello World!')
+    return str(resp)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
