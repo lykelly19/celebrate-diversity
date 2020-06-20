05fb79c4c6db7ade3d6e5330259eddb4622604a2
Kelly <lykelly2014@icloud.com>
2020-06-20 12:47:41 -0400

    retrieves month

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import os
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_sms():
     body = request.values.get('Body', None)
+
+    months = { 1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+               7: 'July', 8: 'August', 9:' September', 10: 'October', 11: 'November', 12: 'December'}
+    today = datetime.datetime.now()   
+    message_string = "The month is {}".format(months[today.month])
+
     resp = MessagingResponse()
-    resp.message(body or 'Hello World!')
+    resp.message(message_string)
     return str(resp)
 
 if __name__ == "__main__":
