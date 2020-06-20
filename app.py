765a892305f38e846967177fd03b1b146051759b
Kelly <lykelly2014@icloud.com>
2020-06-20 15:11:24 -0400

    loads json observances data

--- app.py
+++ app.py
@@ -3,17 +3,30 @@ from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 import datetime
+import json
 
 app = Flask(__name__)
+
+with open('observances_data.json') as json_file:
+    data = json.load(json_file)
+
+months = { 1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+           7: 'July', 8: 'August', 9:' September', 10: 'October', 11: 'November', 12: 'December'}
 
 @app.route('/', methods=['POST'])
 def receive_sms():
     body = request.values.get('Body', None)
 
-    months = { 1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
-               7: 'July', 8: 'August', 9:' September', 10: 'October', 11: 'November', 12: 'December'}
-    today = datetime.datetime.now()   
-    message_string = "The month is {}".format(months[today.month])
+    today = datetime.datetime.now()
+    month = months[today.month]  # convert from the int to word/string representation of a month
+
+    message_string = "The month is {}. Learn more by sending a number to the left of the following options: \n".format(month)
+
+    for i in range(len(data[month]['observances'])):  # print the month's observances
+        message_string += str(i + 1) + ': ' + data[month]['observances'][i] + '\n'
+
+    if(len(data[month]['observances']) == 0):  # if no observances found for the month
+        message_string += 'Learn more about...'
 
     resp = MessagingResponse()
     resp.message(message_string)
